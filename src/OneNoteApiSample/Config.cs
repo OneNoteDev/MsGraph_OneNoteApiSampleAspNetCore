8c70066260de6eb54c9c94b0e4cbd9f79661ecb5
Stefanie Hansen (InConsulting Inc.) <v-stehan@microsoft.com>
2017-03-20 21:20:04 -0700

    Fixed formatting.

--- src/OneNoteApiSample/Config.cs
+++ src/OneNoteApiSample/Config.cs
@@ -6,6 +6,7 @@ namespace OneNoteApiSample
 	{
 		// TODO: Replace all of these with your own
 		// Note: Redirect URIs should be HTTPS, but it is easier to deploy/play with HTTP
+		// TODO: revert back to original URIs.
 		public static string MsaRedirectUri => "http://localhost/msaoauthcallback";
 		public static string MsaAppId => "12fb1c8f-df99-4d53-973c-0730812abd93";
 		public static string MsaAppSecret => "m7rETtQVh2uFsUG91yjTwHD";
@@ -14,14 +15,9 @@ namespace OneNoteApiSample
 		// See https://msdn.microsoft.com/en-us/library/office/dn807159.aspx and https://msdn.microsoft.com/en-us/library/hh243646.aspx#wlbasic
 		public static List<string> MsaRequiredScopes => new List<string>()
 		{
-             "https://graph.microsoft.com/Notes.ReadWrite.All",
-             "openid",
-             "offline_access"
-            
-
-   //         "office.onenote_update",
-			//"wl.basic",
-			//"wl.signin"
+			"https://graph.microsoft.com/Notes.ReadWrite.All",
+			"openid",
+			"offline_access"
 		};
 
 		// TODO: This sample does not support O365 authentication
